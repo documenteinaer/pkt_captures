0ba836492626e2bedeb1b18599c09604cc07b643
Costin Carabaș <costin.carabas@gmail.com>
2022-06-10 20:26:41 +0300

    Adapt json to the new format

--- linux/capture.py
+++ linux/capture.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-duration = 4 # seconds 
+duration = 10 # seconds 
 # ./capture.py > out.json 2> log.txt 
 #
 
@@ -19,7 +19,8 @@ if(platform=="linux"):
 if(platform == "darwin"): 
     tshark_params='/usr/local/bin/tshark -a duration:%d -Ini en1 -T fields -e frame.time_epoch -e wlan.bssid -e wlan_radio.signal_dbm type mgt subtype beacon' % duration
 
-print(tshark_params, file=sys.stderr)
+#print(tshark_params, file=sys.stderr)
+
 
 tshark = subprocess.Popen(shlex.split(tshark_params), stdout=subprocess.PIPE)
 out, err = tshark.communicate()
@@ -33,19 +34,32 @@ now = datetime.now()
 date_time = now.strftime("%d-%m-%Y %H:%M:%S")
 
 wifi_data = {}
+wifi_data['ssid'] = ""
+wifi_data['frequency'] = 0
 wifi_data['rssi'] = []
 for x in out.splitlines():
     pkt = x.decode().split('\t')
-    wifi_data['ssid'] = pkt[3]
-    wifi_data['frequency'] = pkt[1]
-    wifi_data['rssi'].append(int(pkt[4]))
-    hw_addr = pkt[2]
-    wifis[hw_addr] = wifi_data
+    #wifi_data['ssid'] = pkt[3]
+    #wifi_data['frequency'] = pkt[1]
+    #wifi_data['rssi'].append(int(pkt[4]))
+    #hw_addr = pkt[2]
+    #wifis[hw_addr] = wifi_data
+    if not pkt[2] in wifis:
+        wifis[pkt[2]] = {}
+        wifis[pkt[2]]['ssid'] = ""
+        wifis[pkt[2]]['frequency'] = 0
+        wifis[pkt[2]]['rssi'] = []
+    wifis[pkt[2]]['ssid'] = pkt[3]
+    wifis[pkt[2]]['frequency'] = pkt[1]
+    wifis[pkt[2]]['rssi'].append(int(pkt[4]))
     print(pkt, file=sys.stderr)
     
 fingerprint['timestamp'] = date_time
 fingerprint['wifi'] = wifis
 fingerprints.append(fingerprint)
+collection['comment'] = sys.argv[1]
+collection['x'] = 0
+collection['y'] = 0
 collection['fingerprints'] = fingerprints
 collections['collection0'] = collection 
     
